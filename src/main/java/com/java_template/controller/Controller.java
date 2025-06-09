d42b848e091c8cdd636f233c7518511b3e68480c
app-builder <app-builder@example.com>
2025-06-09 21:02:19 +0000

    saved src/main/java/com/java_template/controller/Controller.java: fcf77338-43d6-11b2-ba20-3e7b56738ce4

--- src/main/java/com/java_template/controller/Controller.java
+++ src/main/java/com/java_template/controller/Controller.java
@@ -1,4 +1,4 @@
-package com.java_template.entity;
+package com.java_template.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -17,21 +17,21 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 import static com.java_template.common.config.Config.*;
 
 @RestController
 @RequestMapping("/cyoda/api")
-public class CyodaEntityControllerPrototype {
+public class Controller {
 
     private final EntityService entityService;
-    private final ObjectMapper objectMapper = new ObjectMapper();
-    private static final Logger logger = LoggerFactory.getLogger(CyodaEntityControllerPrototype.class);
+    private final ObjectMapper objectMapper;
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
 
-    public CyodaEntityControllerPrototype(EntityService entityService) {
+    public Controller(EntityService entityService, ObjectMapper objectMapper) {
         this.entityService = entityService;
+        this.objectMapper = objectMapper;
     }
 
     @PostMapping("/data/retrieve")
