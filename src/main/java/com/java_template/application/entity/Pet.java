476ab54d4bca66b99de981f78ee0aca45c9b0e69
Ksenniya <lukkonina1@gmail.com>
2025-07-16 16:41:36 -0300

    saved src/main/java/com/java_template/application/entity/Pet.java: 0f590650-60e1-11b2-a044-564507c7d2b7

---
+++ src/main/java/com/java_template/application/entity/Pet.java
@@ -0,0 +1,32 @@
+package com.java_template.application.entity;
+
+import com.java_template.common.workflow.CyodaEntity;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.common.ModelSpec;
+import lombok.Data;
+import java.util.List;
+import static com.java_template.common.config.Config.ENTITY_VERSION;
+
+@Data
+public class Pet implements CyodaEntity {
+    private Long id;
+    private String name;
+    private String description;
+    private Category category;
+    private List<Tag> tags;
+
+    public Pet() {}
+
+    @Override
+    public OperationSpecification getModelKey() {
+        ModelSpec modelSpec = new ModelSpec();
+        modelSpec.setName("pet");
+        modelSpec.setVersion(Integer.parseInt(ENTITY_VERSION));
+        return new OperationSpecification.Entity(modelSpec, "pet");
+    }
+
+    @Override
+    public boolean isValid() {
+        return name != null && !name.trim().isEmpty();
+    }
+}
