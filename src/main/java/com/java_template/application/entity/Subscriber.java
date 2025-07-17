275f25974dd2e575bef4ad4e59a71b08dcd02079
app-builder <app-builder@example.com>
2025-07-17 14:20:04 +0000

    saved src/main/java/com/java_template/application/entity/Subscriber.java: 20ae6d28-2afe-11b2-9aa9-82c173d627ff

---
+++ src/main/java/com/java_template/application/entity/Subscriber.java
@@ -0,0 +1,36 @@
+package com.java_template.application.entity;
+
+import com.java_template.common.workflow.CyodaEntity;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.common.ModelSpec;
+import lombok.Data;
+import static com.java_template.common.config.Config.ENTITY_VERSION;
+
+@Data
+public class Subscriber implements CyodaEntity {
+    private final String email;
+    private final String name;
+
+    public Subscriber() {
+        this.email = null;
+        this.name = null;
+    }
+
+    public Subscriber(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+
+    @Override
+    public OperationSpecification getModelKey() {
+        ModelSpec modelSpec = new ModelSpec();
+        modelSpec.setName("subscriber");
+        modelSpec.setVersion(Integer.parseInt(ENTITY_VERSION));
+        return new OperationSpecification.Entity(modelSpec, "subscriber");
+    }
+
+    @Override
+    public boolean isValid() {
+        return email != null && !email.isEmpty();
+    }
+}
