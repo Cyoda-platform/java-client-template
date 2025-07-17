a083cfb558c131e8e934584df89de5de1e3066e0
app-builder <app-builder@example.com>
2025-07-17 14:20:37 +0000

    saved src/main/java/com/java_template/application/processor/SubscriberProcessor.java: 20ae6d28-2afe-11b2-9aa9-82c173d627ff

---
+++ src/main/java/com/java_template/application/processor/SubscriberProcessor.java
@@ -0,0 +1,51 @@
+package com.java_template.application.processor;
+
+import com.java_template.application.entity.Subscriber;
+import com.java_template.common.serializer.ErrorInfo;
+import com.java_template.common.serializer.ProcessorSerializer;
+import com.java_template.common.serializer.SerializerFactory;
+import com.java_template.common.workflow.CyodaEventContext;
+import com.java_template.common.workflow.CyodaProcessor;
+import com.java_template.common.workflow.OperationSpecification;
+import com.java_template.common.config.Config;
+import org.cyoda.cloud.api.event.processing.EntityProcessorCalculationRequest;
+import org.cyoda.cloud.api.event.processing.EntityProcessorCalculationResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SubscriberProcessor implements CyodaProcessor {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final ProcessorSerializer serializer;
+
+    public SubscriberProcessor(SerializerFactory serializerFactory) {
+        this.serializer = serializerFactory.getDefaultProcessorSerializer();
+        logger.info("SubscriberProcessor initialized with SerializerFactory");
+    }
+
+    @Override
+    public EntityProcessorCalculationResponse process(CyodaEventContext<EntityProcessorCalculationRequest> context) {
+        EntityProcessorCalculationRequest request = context.getEvent();
+        logger.info("Processing Subscriber for request: {}", request.getId());
+
+        return serializer.withRequest(request)
+                .toEntity(Subscriber.class)
+                .withErrorHandler(this::handleSubscriberError)
+                .validate(Subscriber::isValid, "Invalid Subscriber state")
+                .complete();
+    }
+
+    @Override
+    public boolean supports(OperationSpecification modelSpec) {
+        return "SubscriberProcessor".equals(modelSpec.operationName()) &&
+                "subscriber".equals(modelSpec.modelKey().getName()) &&
+                Integer.parseInt(Config.ENTITY_VERSION) == modelSpec.modelKey().getVersion();
+    }
+
+    private ErrorInfo handleSubscriberError(Throwable throwable, Subscriber subscriber) {
+        logger.error("Error processing Subscriber", throwable);
+        return new ErrorInfo("Subscriber processing error", throwable.getMessage());
+    }
+}
