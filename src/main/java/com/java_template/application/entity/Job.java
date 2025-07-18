b05efe263fe2d82b4e3083053e25ffd8d07b9cd9
app-builder <app-builder@example.com>
2025-07-18 00:47:38 +0000

    saved src/main/java/com/java_template/application/entity/Job.java: 0718ef00-61d6-11b2-8dea-c60f39218cef

---
+++ src/main/java/com/java_template/application/entity/Job.java
@@ -0,0 +1,34 @@
+package com.java_template.application.entity;
+
+import com.java_template.common.workflow.CyodaEntity;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.common.ModelSpec;
+import lombok.Data;
+import java.util.UUID;
+import static com.java_template.common.config.Config.ENTITY_VERSION;
+
+@Data
+public class Job implements CyodaEntity {
+    private String id; // business ID
+    private UUID technicalId; // database ID
+
+    private String type; // FETCH_SCORES or SEND_NOTIFICATIONS
+    private String status; // PENDING or COMPLETED
+
+    public Job() {}
+
+    @Override
+    public OperationSpecification getModelKey() {
+        ModelSpec modelSpec = new ModelSpec();
+        modelSpec.setName("job");
+        modelSpec.setVersion(Integer.parseInt(ENTITY_VERSION));
+        return new OperationSpecification.Entity(modelSpec, "job");
+    }
+
+    @Override
+    public boolean isValid() {
+        return (id != null && !id.isBlank()) &&
+               (type != null && !type.isBlank()) &&
+               (status != null && !status.isBlank());
+    }
+}
