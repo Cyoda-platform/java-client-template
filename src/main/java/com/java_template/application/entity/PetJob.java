285010661fdde9d1fb94472bae136145228641f1
app-builder <app-builder@example.com>
2025-07-18 18:56:44 +0000

    saved src/main/java/com/java_template/application/entity/PetJob.java: 5b45b26e-2bf0-11b2-9aa9-82c173d627ff

---
+++ src/main/java/com/java_template/application/entity/PetJob.java
@@ -0,0 +1,40 @@
+package com.java_template.application.entity;
+
+import com.java_template.common.workflow.CyodaEntity;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.common.ModelSpec;
+import lombok.Data;
+import static com.java_template.common.config.Config.ENTITY_VERSION;
+
+import java.util.UUID;
+
+@Data
+public class PetJob implements CyodaEntity {
+    private String id; // business ID
+    private UUID technicalId; // database ID
+
+    private String jobId;
+    private String action;
+    private Long petId;
+    private String status;
+
+    public PetJob() {}
+
+    @Override
+    public OperationSpecification getModelKey() {
+        ModelSpec modelSpec = new ModelSpec();
+        modelSpec.setName("petJob");
+        modelSpec.setVersion(Integer.parseInt(ENTITY_VERSION));
+        return new OperationSpecification.Entity(modelSpec, "petJob");
+    }
+
+    @Override
+    public boolean isValid() {
+        return id != null && !id.isBlank()
+            && technicalId != null
+            && jobId != null && !jobId.isBlank()
+            && action != null && !action.isBlank()
+            && petId != null
+            && status != null && !status.isBlank();
+    }
+}
