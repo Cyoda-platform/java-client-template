210684cc3809e96cbb41e2176edcdd4ded27a6ad
app-builder <app-builder@example.com>
2025-07-19 01:35:21 +0000

    saved src/main/java/com/java_template/application/criterion/UpdatePetStatusActionCriterion.java: 2bcb0076-62a4-11b2-965a-2e0ad7d884c5

---
+++ src/main/java/com/java_template/application/criterion/UpdatePetStatusActionCriterion.java
@@ -0,0 +1,53 @@
+package com.java_template.application.criterion;
+
+import com.java_template.application.entity.PurrfectPetsJob;
+import com.java_template.common.serializer.CriterionSerializer;
+import com.java_template.common.serializer.EvaluationOutcome;
+import com.java_template.common.serializer.ReasonAttachmentStrategy;
+import com.java_template.common.serializer.SerializerFactory;
+import com.java_template.common.serializer.StandardEvalReasonCategories;
+import com.java_template.common.config.Config;
+import com.java_template.common.workflow.CyodaCriterion;
+import com.java_template.common.workflow.CyodaEventContext;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.processing.EntityCriteriaCalculationRequest;
+import org.cyoda.cloud.api.event.processing.EntityCriteriaCalculationResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdatePetStatusActionCriterion implements CyodaCriterion {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final CriterionSerializer serializer;
+
+    public UpdatePetStatusActionCriterion(SerializerFactory serializerFactory) {
+        this.serializer = serializerFactory.getDefaultCriteriaSerializer();
+        logger.info("UpdatePetStatusActionCriterion initialized with SerializerFactory");
+    }
+
+    @Override
+    public EntityCriteriaCalculationResponse check(CyodaEventContext<EntityCriteriaCalculationRequest> context) {
+        EntityCriteriaCalculationRequest request = context.getEvent();
+
+        return serializer.withRequest(request)
+                .evaluateEntity(PurrfectPetsJob.class, this::validateEntity)
+                .withReasonAttachment(ReasonAttachmentStrategy.toWarnings())
+                .complete();
+    }
+
+    @Override
+    public boolean supports(OperationSpecification modelSpec) {
+        return "UpdatePetStatusActionCriterion".equals(modelSpec.operationName()) &&
+                "purrfectPetsJob".equalsIgnoreCase(modelSpec.modelKey().getName()) &&
+                Integer.parseInt(Config.ENTITY_VERSION) == modelSpec.modelKey().getVersion();
+    }
+
+    private EvaluationOutcome validateEntity(PurrfectPetsJob entity) {
+        if (!"UPDATE_PET_STATUS".equals(entity.getActionType())) {
+            return EvaluationOutcome.fail("ActionType must be UPDATE_PET_STATUS", StandardEvalReasonCategories.VALIDATION_FAILURE);
+        }
+        return EvaluationOutcome.success();
+    }
+}
