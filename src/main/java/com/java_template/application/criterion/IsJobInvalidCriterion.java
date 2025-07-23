7d753ea30716d2994b3ba183ab8c2ca470ed4282
app-builder <app-builder@example.com>
2025-07-23 14:06:50 +0000

    saved src/main/java/com/java_template/application/criterion/IsJobInvalidCriterion.java: e544957c-6631-11b2-95b2-ca2f093e3d5b

---
+++ src/main/java/com/java_template/application/criterion/IsJobInvalidCriterion.java
@@ -0,0 +1,54 @@
+package com.java_template.application.criterion;
+
+import com.java_template.application.entity.PetRegistrationJob;
+import com.java_template.common.serializer.CriterionSerializer;
+import com.java_template.common.serializer.EvaluationOutcome;
+import com.java_template.common.serializer.ReasonAttachmentStrategy;
+import com.java_template.common.serializer.SerializerFactory;
+import com.java_template.common.serializer.StandardEvalReasonCategories;
+import com.java_template.common.config.Config;
+import com.java_template.common.workflow.CyodaCriterion;
+import com.java_template.common.workflow.CyodaEventContext;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.processing.EntityCriteriaCalculationRequest;
+import org.cyoda.cloud.api.event.processing.EntityCriteriaCalculationResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IsJobInvalidCriterion implements CyodaCriterion {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final CriterionSerializer serializer;
+
+    public IsJobInvalidCriterion(SerializerFactory serializerFactory) {
+        this.serializer = serializerFactory.getDefaultCriteriaSerializer();
+        logger.info("IsJobInvalidCriterion initialized with SerializerFactory");
+    }
+
+    @Override
+    public EntityCriteriaCalculationResponse check(CyodaEventContext<EntityCriteriaCalculationRequest> context) {
+        EntityCriteriaCalculationRequest request = context.getEvent();
+
+        return serializer.withRequest(request)
+            .evaluateEntity(PetRegistrationJob.class, this::validateEntity)
+            .withReasonAttachment(ReasonAttachmentStrategy.toWarnings())
+            .complete();
+    }
+
+    @Override
+    public boolean supports(OperationSpecification modelSpec) {
+        return "IsJobInvalidCriterion".equals(modelSpec.operationName()) &&
+               "petRegistrationJob".equalsIgnoreCase(modelSpec.modelKey().getName()) &&
+               Integer.parseInt(Config.ENTITY_VERSION) == modelSpec.modelKey().getVersion();
+    }
+
+    private EvaluationOutcome validateEntity(PetRegistrationJob entity) {
+        // Business logic: job must be invalid (fails isValid())
+        if (entity.isValid()) {
+            return EvaluationOutcome.fail("Job is valid", StandardEvalReasonCategories.VALIDATION_FAILURE);
+        }
+        return EvaluationOutcome.success();
+    }
+}
