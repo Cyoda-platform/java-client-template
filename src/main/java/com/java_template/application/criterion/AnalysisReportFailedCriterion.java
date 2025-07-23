05df4dc748c6dcc8a0ef413d7927f031a1eb712e
app-builder <app-builder@example.com>
2025-07-23 21:18:12 +0000

    saved src/main/java/com/java_template/application/criterion/AnalysisReportFailedCriterion.java: e09bb2ec-2f6f-11b2-944c-fac101f95347

---
+++ src/main/java/com/java_template/application/criterion/AnalysisReportFailedCriterion.java
@@ -0,0 +1,56 @@
+package com.java_template.application.criterion;
+
+import com.java_template.application.entity.CommentAnalysisReport;
+import com.java_template.common.serializer.CriterionSerializer;
+import com.java_template.common.serializer.EvaluationOutcome;
+import com.java_template.common.serializer.ReasonAttachmentStrategy;
+import com.java_template.common.serializer.SerializerFactory;
+import com.java_template.common.serializer.StandardEvalReasonCategories;
+import com.java_template.common.config.Config;
+import com.java_template.common.workflow.CyodaCriterion;
+import com.java_template.common.workflow.CyodaEventContext;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.processing.EntityCriteriaCalculationRequest;
+import org.cyoda.cloud.api.event.processing.EntityCriteriaCalculationResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnalysisReportFailedCriterion implements CyodaCriterion {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final CriterionSerializer serializer;
+
+    public AnalysisReportFailedCriterion(SerializerFactory serializerFactory) {
+        this.serializer = serializerFactory.getDefaultCriteriaSerializer();
+        logger.info("AnalysisReportFailedCriterion initialized with SerializerFactory");
+    }
+
+    @Override
+    public EntityCriteriaCalculationResponse check(CyodaEventContext<EntityCriteriaCalculationRequest> context) {
+        EntityCriteriaCalculationRequest request = context.getEvent();
+
+        return serializer.withRequest(request)
+            .evaluateEntity(CommentAnalysisReport.class, this::validateEntity)
+            .withReasonAttachment(ReasonAttachmentStrategy.toWarnings())
+            .complete();
+    }
+
+    @Override
+    public boolean supports(OperationSpecification modelSpec) {
+        return "AnalysisReportFailedCriterion".equals(modelSpec.operationName()) &&
+               "commentanalysisreport".equalsIgnoreCase(modelSpec.modelKey().getName()) &&
+               Integer.parseInt(Config.ENTITY_VERSION) == modelSpec.modelKey().getVersion();
+    }
+
+    private EvaluationOutcome validateEntity(CommentAnalysisReport entity) {
+        if (entity == null) {
+            return EvaluationOutcome.fail("Report does not exist", StandardEvalReasonCategories.DATA_QUALITY_FAILURE);
+        }
+        if (!"CREATED".equalsIgnoreCase(entity.getStatus())) {
+            return EvaluationOutcome.fail("Report status is not CREATED", StandardEvalReasonCategories.BUSINESS_RULE_FAILURE);
+        }
+        return EvaluationOutcome.success();
+    }
+}
