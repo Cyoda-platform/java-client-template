2ea3474090a44638ddc34bce0697abc33cc7d6cd
app-builder <app-builder@example.com>
2025-07-24 19:05:23 +0000

    saved src/main/java/com/java_template/application/processor/PetRegistrationJobValidCriterion.java: 38a79ff2-30a4-11b2-ac6a-1297eff5fbfd

---
+++ src/main/java/com/java_template/application/processor/PetRegistrationJobValidCriterion.java
@@ -0,0 +1,30 @@
+package com.java_template.application.processor;
+
+import com.java_template.common.workflow.CyodaProcessor;
+import com.java_template.common.workflow.CyodaEventContext;
+import com.java_template.common.workflow.OperationSpecification;
+import com.java_template.common.config.Config;
+import org.cyoda.cloud.api.event.processing.EntityProcessorCalculationRequest;
+import org.cyoda.cloud.api.event.processing.EntityProcessorCalculationResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PetRegistrationJobValidCriterion implements CyodaProcessor {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public EntityProcessorCalculationResponse process(CyodaEventContext<EntityProcessorCalculationRequest> context) {
+        // Criterion processors are ignored as per instructions
+        return null;
+    }
+
+    @Override
+    public boolean supports(OperationSpecification modelSpec) {
+        return "PetRegistrationJobValidCriterion".equals(modelSpec.operationName()) &&
+                "petregistrationjob".equalsIgnoreCase(modelSpec.modelKey().getName()) &&
+                Integer.parseInt(Config.ENTITY_VERSION) == modelSpec.modelKey().getVersion();
+    }
+}
