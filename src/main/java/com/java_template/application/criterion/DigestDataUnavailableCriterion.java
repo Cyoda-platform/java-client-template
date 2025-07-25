c64593fc97acf3ea46e86f02476a2f6b3abae441
app-builder <app-builder@example.com>
2025-07-25 16:10:25 +0000

    saved src/main/java/com/java_template/application/criterion/DigestDataUnavailableCriterion.java: 4a8ff9c6-49db-11b2-af30-2a39b88383d3

--- src/main/java/com/java_template/application/criterion/DigestDataUnavailableCriterion.java
+++ src/main/java/com/java_template/application/criterion/DigestDataUnavailableCriterion.java
@@ -32,21 +32,21 @@ public class DigestDataUnavailableCriterion implements CyodaCriterion {
         EntityCriteriaCalculationRequest request = context.getEvent();
 
         return serializer.withRequest(request)
-            .evaluateEntity(DigestData.class, this::validateEntity)
-            .withReasonAttachment(ReasonAttachmentStrategy.toWarnings())
-            .complete();
+                .evaluateEntity(DigestData.class, this::validateEntity)
+                .withReasonAttachment(ReasonAttachmentStrategy.toWarnings())
+                .complete();
     }
 
     @Override
     public boolean supports(OperationSpecification modelSpec) {
         return "DigestDataUnavailableCriterion".equals(modelSpec.operationName()) &&
-               "digestData".equalsIgnoreCase(modelSpec.modelKey().getName()) &&
-               Integer.parseInt(Config.ENTITY_VERSION) == modelSpec.modelKey().getVersion();
+                "digestData".equalsIgnoreCase(modelSpec.modelKey().getName()) &&
+                Integer.parseInt(Config.ENTITY_VERSION) == modelSpec.modelKey().getVersion();
     }
 
     private EvaluationOutcome validateEntity(DigestData entity) {
         if (entity.getRetrievedData() != null && !entity.getRetrievedData().isBlank()) {
-            return EvaluationOutcome.fail("Retrieved data should be empty", StandardEvalReasonCategories.VALIDATION_FAILURE);
+            return EvaluationOutcome.fail("Retrieved data should be missing", StandardEvalReasonCategories.DATA_QUALITY_FAILURE);
         }
         return EvaluationOutcome.success();
     }
