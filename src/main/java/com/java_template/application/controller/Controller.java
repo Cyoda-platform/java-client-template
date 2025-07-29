90a0a5e677e2a6e7ea0fd85a7ad320ff3ca55e65
app-builder <app-builder@example.com>
2025-07-29 15:43:15 +0000

    saved src/main/java/com/java_template/application/controller/Controller.java: ab2cb350-33fb-11b2-a608-c21a3d86216a

--- src/main/java/com/java_template/application/controller/Controller.java
+++ src/main/java/com/java_template/application/controller/Controller.java
@@ -42,7 +42,7 @@ public class Controller {
 
             log.info("Mail created with technicalId {}", technicalId);
 
-            processMail(technicalId.toString(), mail);
+            // processMail method removed for workflow extraction
 
             Map<String, String> response = new HashMap<>();
             response.put("technicalId", technicalId.toString());
@@ -90,46 +90,5 @@ public class Controller {
         }
     }
 
-    private void processMail(String technicalId, Mail mail) {
-        log.info("Processing mail with technicalId {}", technicalId);
-
-        if (Boolean.TRUE.equals(mail.getIsHappy())) {
-            if (checkMailHappy(mail)) {
-                sendHappyMail(technicalId, mail);
-            } else {
-                log.error("Mail failed happy criteria check: {}", technicalId);
-            }
-        } else {
-            if (checkMailGloomy(mail)) {
-                sendGloomyMail(technicalId, mail);
-            } else {
-                log.error("Mail failed gloomy criteria check: {}", technicalId);
-            }
-        }
-    }
-
-    private boolean checkMailHappy(Mail mail) {
-        // Criteria: mail isHappy == true
-        return Boolean.TRUE.equals(mail.getIsHappy());
-    }
-
-    private boolean checkMailGloomy(Mail mail) {
-        // Criteria: mail isHappy == false
-        return Boolean.FALSE.equals(mail.getIsHappy());
-    }
-
-    private void sendHappyMail(String technicalId, Mail mail) {
-        // Simulate sending happy mail
-        log.info("Sending happy mail to {} with subject '{}'", mail.getMailList(), mail.getSubject());
-        // Here could be integration with JavaMailSender or external mail API
-        // For prototype, we just log success
-        log.info("Happy mail sent successfully for technicalId {}", technicalId);
-    }
-
-    private void sendGloomyMail(String technicalId, Mail mail) {
-        // Simulate sending gloomy mail
-        log.info("Sending gloomy mail to {} with subject '{}'", mail.getMailList(), mail.getSubject());
-        // For prototype, we just log success
-        log.info("Gloomy mail sent successfully for technicalId {}", technicalId);
-    }
+    // Removed processMail and its helper methods for workflow extraction
 }
