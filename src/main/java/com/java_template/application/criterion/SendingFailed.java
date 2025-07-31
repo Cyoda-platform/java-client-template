d6b2cb8f7756d9e7f4af0ddb74d2b3d01d5439d9
Ksenniya <lukkonina1@gmail.com>
2025-07-31 13:55:33 -0300

    saved src/main/java/com/java_template/application/criterion/SendingFailed.java: a2063c5e-6c92-11b2-bf25-d2deaf18d561

---
+++ src/main/java/com/java_template/application/criterion/SendingFailed.java
@@ -0,0 +1,52 @@
+package com.java_template.application.criterion;
+
+import com.java_template.application.entity.Mail;
+import com.java_template.common.serializer.CriterionSerializer;
+import com.java_template.common.serializer.EvaluationOutcome;
+import com.java_template.common.serializer.ReasonAttachmentStrategy;
+import com.java_template.common.serializer.SerializerFactory;
+import com.java_template.common.serializer.StandardEvalReasonCategories;
+import com.java_template.common.config.Config;
+import com.java_template.common.workflow.CyodaCriterion;
+import com.java_template.common.workflow.CyodaEventContext;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.processing.EntityCriteriaCalculationRequest;
+import org.cyoda.cloud.api.event.processing.EntityCriteriaCalculationResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SendingFailed implements CyodaCriterion {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final CriterionSerializer serializer;
+    private final String className = this.getClass().getSimpleName();
+
+    public SendingFailed(SerializerFactory serializerFactory) {
+        this.serializer = serializerFactory.getDefaultCriteriaSerializer();
+    }
+
+    @Override
+    public EntityCriteriaCalculationResponse check(CyodaEventContext<EntityCriteriaCalculationRequest> context) {
+        EntityCriteriaCalculationRequest request = context.getEvent();
+        return serializer.withRequest(request)
+            .evaluateEntity(Mail.class, this::validateEntity)
+            .withReasonAttachment(ReasonAttachmentStrategy.toWarnings())
+            .complete();
+    }
+
+    @Override
+    public boolean supports(OperationSpecification modelSpec) {
+        return className.equalsIgnoreCase(modelSpec.operationName());
+    }
+
+    private EvaluationOutcome validateEntity(CriterionSerializer.CriterionEntityEvaluationContext<Mail> context) {
+        Mail mail = context.entity();
+
+        // Assuming sending failure is indicated by some status or flag in Mail entity
+        // Since no such property exists in Mail entity, we just fail with a message.
+
+        return EvaluationOutcome.fail("Sending failed condition triggered", StandardEvalReasonCategories.BUSINESS_RULE_FAILURE);
+    }
+}
