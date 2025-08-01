05885818457ece2fb924bba6acdbb536b8fa76ce
Ksenniya <lukkonina1@gmail.com>
2025-08-01 17:27:39 -0300

    saved src/main/java/com/java_template/application/entity/Mail.java: d428aada-36fc-11b2-b3c2-2a2c9fd0c6d8

---
+++ src/main/java/com/java_template/application/entity/Mail.java
@@ -0,0 +1,43 @@
+package com.java_template.application.entity;
+
+import com.java_template.common.workflow.CyodaEntity;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.common.ModelSpec;
+import lombok.Data;
+import static com.java_template.common.config.Config.ENTITY_VERSION;
+
+import java.util.List;
+
+@Data
+public class Mail implements CyodaEntity {
+    public static final String ENTITY_NAME = "Mail";
+
+    private Boolean isHappy;
+    private List<String> mailList;
+
+    public Mail() {}
+
+    @Override
+    public OperationSpecification getModelKey() {
+        ModelSpec modelSpec = new ModelSpec();
+        modelSpec.setName(ENTITY_NAME);
+        modelSpec.setVersion(Integer.parseInt(ENTITY_VERSION));
+        return new OperationSpecification.Entity(modelSpec, ENTITY_NAME);
+    }
+
+    @Override
+    public boolean isValid() {
+        if (isHappy == null) {
+            return false;
+        }
+        if (mailList == null || mailList.isEmpty()) {
+            return false;
+        }
+        for (String mail : mailList) {
+            if (mail == null || mail.isBlank()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
