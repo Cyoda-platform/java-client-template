466635625e5039c2933f0aa5a54153c07c938616
app-builder <app-builder@example.com>
2025-08-05 19:36:28 +0000

    saved src/main/java/com/java_template/application/entity/Pet.java: b829fe36-399c-11b2-a608-c21a3d86216a

---
+++ src/main/java/com/java_template/application/entity/Pet.java
@@ -0,0 +1,37 @@
+package com.java_template.application.entity;
+
+import com.java_template.common.workflow.CyodaEntity;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.common.ModelSpec;
+import lombok.Data;
+import static com.java_template.common.config.Config.ENTITY_VERSION;
+
+import java.util.List;
+
+@Data
+public class Pet implements CyodaEntity {
+    public static final String ENTITY_NAME = "Pet";
+
+    private String name;
+    private String category;
+    private List<String> photoUrls;
+    private List<String> tags;
+    private String status;
+
+    public Pet() {}
+
+    @Override
+    public OperationSpecification getModelKey() {
+        ModelSpec modelSpec = new ModelSpec();
+        modelSpec.setName(ENTITY_NAME);
+        modelSpec.setVersion(Integer.parseInt(ENTITY_VERSION));
+        return new OperationSpecification.Entity(modelSpec, ENTITY_NAME);
+    }
+
+    @Override
+    public boolean isValid() {
+        return name != null && !name.isBlank()
+                && category != null && !category.isBlank()
+                && status != null && !status.isBlank();
+    }
+}
