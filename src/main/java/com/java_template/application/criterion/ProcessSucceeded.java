22e7a89a6557bcceff2f0bc8dca3ec538e7f7728
app-builder <app-builder@example.com>
2025-08-05 16:28:30 +0000

    saved src/main/java/com/java_template/application/criterion/ProcessSucceeded.java: 6e5c2892-5283-11b2-af30-2a39b88383d3

---
+++ src/main/java/com/java_template/application/criterion/ProcessSucceeded.java
@@ -0,0 +1,55 @@
+package com.java_template.application.criterion;
+
+import com.java_template.application.entity.Mail;
+import com.java_template.common.serializer.CriterionSerializer;
+import com.java_template.common.serializer.EvaluationOutcome;
+import com.java_template.common.serializer.ReasonAttachmentStrategy;
+import com.java_template.common.serializer.SerializerFactory;
+import com.java_template.common.serializer.StandardEvalReasonCategories;
+import com.java_template.common.config.Config;
+import com.java_template.common.workflow.CyodaCriterion;
+import com.java_template.common.workflow.CyodaEventContext;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.processing.EntityCriteriaCalculationRequest;
+import org.cyoda.cloud.api.event.processing.EntityCriteriaCalculationResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProcessSucceeded implements CyodaCriterion {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final CriterionSerializer serializer;
+    private final String className = this.getClass().getSimpleName();
+
+    public ProcessSucceeded(SerializerFactory serializerFactory) {
+        this.serializer = serializerFactory.getDefaultCriteriaSerializer();
+    }
+
+    @Override
+    public EntityCriteriaCalculationResponse check(CyodaEventContext<EntityCriteriaCalculationRequest> context) {
+        EntityCriteriaCalculationRequest request = context.getEvent();
+        return serializer.withRequest(request)
+            .evaluateEntity(Mail.class, this::validateEntity)
+            .withReasonAttachment(ReasonAttachmentStrategy.toWarnings())
+            .complete();
+    }
+
+    @Override
+    public boolean supports(OperationSpecification modelSpec) {
+        return className.equalsIgnoreCase(modelSpec.operationName());
+    }
+
+    private EvaluationOutcome validateEntity(CriterionSerializer.CriterionEntityEvaluationContext<Mail> context) {
+
+        Mail mail = context.entity();
+
+        // ProcessSucceeded means sending succeeded if mail is happy
+        if (!mail.isIsHappy()) {
+            return EvaluationOutcome.fail("Mail is not happy, processing did not succeed", StandardEvalReasonCategories.BUSINESS_RULE_FAILURE);
+        }
+
+        return EvaluationOutcome.success();
+    }
+}
