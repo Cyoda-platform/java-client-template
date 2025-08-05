a3d6eab4c3a45a7513e5b94d4d7f14cf3924f5ad
Ksenniya <lukkonina1@gmail.com>
2025-08-05 14:51:40 -0300

    saved src/main/java/com/java_template/application/entity/FunPetMatch.java: 19ebc84e-3988-11b2-8f09-b25949731c13

---
+++ src/main/java/com/java_template/application/entity/FunPetMatch.java
@@ -0,0 +1,32 @@
+package com.java_template.application.entity;
+
+import com.java_template.common.workflow.CyodaEntity;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.common.ModelSpec;
+import lombok.Data;
+import static com.java_template.common.config.Config.ENTITY_VERSION;
+
+@Data
+public class FunPetMatch implements CyodaEntity {
+    public static final String ENTITY_NAME = "FunPetMatch";
+
+    private Long petId;
+    private Double matchScore;
+    private String criteria;
+    private String createdAt;
+
+    public FunPetMatch() {}
+
+    @Override
+    public OperationSpecification getModelKey() {
+        ModelSpec modelSpec = new ModelSpec();
+        modelSpec.setName(ENTITY_NAME);
+        modelSpec.setVersion(Integer.parseInt(ENTITY_VERSION));
+        return new OperationSpecification.Entity(modelSpec, ENTITY_NAME);
+    }
+
+    @Override
+    public boolean isValid() {
+        return criteria != null && !criteria.isBlank();
+    }
+}
