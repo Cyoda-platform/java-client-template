ce488fe70aca1a12dbcfe4737203c7d41518bc17
Ksenniya <lukkonina1@gmail.com>
2025-08-06 12:29:47 -0300

    saved src/main/java/com/java_template/application/entity/Workflow.java: a5b7149a-3a3d-11b2-8f09-b25949731c13

---
+++ src/main/java/com/java_template/application/entity/Workflow.java
@@ -0,0 +1,35 @@
+package com.java_template.application.entity;
+
+import com.java_template.common.workflow.CyodaEntity;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.common.ModelSpec;
+import lombok.Data;
+import static com.java_template.common.config.Config.ENTITY_VERSION;
+
+@Data
+public class Workflow implements CyodaEntity {
+    public static final String ENTITY_NAME = "Workflow";
+
+    private String name;
+    private String description;
+    private String status;
+    private String createdAt;
+    private String petId;
+
+    public Workflow() {}
+
+    @Override
+    public OperationSpecification getModelKey() {
+        ModelSpec modelSpec = new ModelSpec();
+        modelSpec.setName(ENTITY_NAME);
+        modelSpec.setVersion(Integer.parseInt(ENTITY_VERSION));
+        return new OperationSpecification.Entity(modelSpec, ENTITY_NAME);
+    }
+
+    @Override
+    public boolean isValid() {
+        return name != null && !name.isBlank()
+            && status != null && !status.isBlank()
+            && petId != null && !petId.isBlank();
+    }
+}
