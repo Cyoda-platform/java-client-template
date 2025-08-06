30367cf4b84983dc6462482f76839b99f5ccce8b
app-builder <app-builder@example.com>
2025-08-06 03:27:51 +0000

    saved src/main/java/com/java_template/application/entity/HackerNewsItem.java: 7d81414e-52de-11b2-af30-2a39b88383d3

---
+++ src/main/java/com/java_template/application/entity/HackerNewsItem.java
@@ -0,0 +1,39 @@
+package com.java_template.application.entity;
+
+import com.java_template.common.workflow.CyodaEntity;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.common.ModelSpec;
+import lombok.Data;
+import static com.java_template.common.config.Config.ENTITY_VERSION;
+
+@Data
+public class HackerNewsItem implements CyodaEntity {
+    public static final String ENTITY_NAME = "HackerNewsItem";
+
+    private Long id;
+    private String type;
+    private String originalJson;
+    private String importTimestamp; // ISO-8601 format
+    private String state; // VALID or INVALID
+
+    public HackerNewsItem() {}
+
+    @Override
+    public OperationSpecification getModelKey() {
+        ModelSpec modelSpec = new ModelSpec();
+        modelSpec.setName(ENTITY_NAME);
+        modelSpec.setVersion(Integer.parseInt(ENTITY_VERSION));
+        return new OperationSpecification.Entity(modelSpec, ENTITY_NAME);
+    }
+
+    @Override
+    public boolean isValid() {
+        if (id == null) {
+            return false;
+        }
+        if (type == null || type.isBlank()) {
+            return false;
+        }
+        return true;
+    }
+}
