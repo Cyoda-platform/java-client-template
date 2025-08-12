552643d5f3aa23c485bdd5188d845930e816849f
Ksenniya <lukkonina1@gmail.com>
2025-08-12 20:16:07 -0300

    saved src/main/java/com/java_template/application/entity/laureate/version_1/Laureate.java: 38b1003c-7637-11b2-b0f6-36a84c9cbd5c

---
+++ src/main/java/com/java_template/application/entity/laureate/version_1/Laureate.java
@@ -0,0 +1,46 @@
+package com.java_template.application.entity.laureate.version_1;
+
+import com.java_template.common.workflow.CyodaEntity;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.common.ModelSpec;
+import lombok.Data;
+
+@Data
+public class Laureate implements CyodaEntity {
+    public static final String ENTITY_NAME = "Laureate";
+    public static final Integer ENTITY_VERSION = 1;
+
+    private Integer id;
+    private String firstname;
+    private String surname;
+    private String gender;
+    private String born;
+    private String died;
+    private String borncountry;
+    private String borncountrycode;
+    private String borncity;
+    private String year;
+    private String category;
+    private String motivation;
+    private String name;
+    private String city;
+    private String country;
+
+    public Laureate() {}
+
+    @Override
+    public OperationSpecification getModelKey() {
+        ModelSpec modelSpec = new ModelSpec();
+        modelSpec.setName(ENTITY_NAME);
+        modelSpec.setVersion(ENTITY_VERSION);
+        return new OperationSpecification.Entity(modelSpec, ENTITY_NAME);
+    }
+
+    @Override
+    public boolean isValid() {
+        return firstname != null && !firstname.isBlank() &&
+               surname != null && !surname.isBlank() &&
+               year != null && !year.isBlank() &&
+               category != null && !category.isBlank();
+    }
+}
