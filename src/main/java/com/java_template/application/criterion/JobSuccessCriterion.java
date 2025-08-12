8b81842c7a4b797b30d578514149635f2ed0935d
Ksenniya <lukkonina1@gmail.com>
2025-08-12 11:04:59 -0300

    saved src/main/java/com/java_template/application/criterion/JobSuccessCriterion.java: eab6516a-75e7-11b2-b0f6-36a84c9cbd5c

---
+++ src/main/java/com/java_template/application/criterion/JobSuccessCriterion.java
@@ -0,0 +1,54 @@
+package com.java_template.application.criterion;
+
+import com.java_template.application.entity.job.version_1.Job;
+import com.java_template.common.serializer.CriterionSerializer;
+import com.java_template.common.serializer.EvaluationOutcome;
+import com.java_template.common.serializer.ReasonAttachmentStrategy;
+import com.java_template.common.serializer.SerializerFactory;
+import com.java_template.common.serializer.StandardEvalReasonCategories;
+import com.java_template.common.workflow.CyodaCriterion;
+import com.java_template.common.workflow.CyodaEventContext;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.processing.EntityCriteriaCalculationRequest;
+import org.cyoda.cloud.api.event.processing.EntityCriteriaCalculationResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobSuccessCriterion implements CyodaCriterion {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final CriterionSerializer serializer;
+    private final String className = this.getClass().getSimpleName();
+
+    public JobSuccessCriterion(SerializerFactory serializerFactory) {
+        this.serializer = serializerFactory.getDefaultCriteriaSerializer();
+    }
+
+    @Override
+    public EntityCriteriaCalculationResponse check(CyodaEventContext<EntityCriteriaCalculationRequest> context) {
+        EntityCriteriaCalculationRequest request = context.getEvent();
+        return serializer.withRequest(request)
+            .evaluateEntity(Job.class, this::validateEntity)
+            .withReasonAttachment(ReasonAttachmentStrategy.toWarnings())
+            .complete();
+    }
+
+    @Override
+    public boolean supports(OperationSpecification modelSpec) {
+        return className.equalsIgnoreCase(modelSpec.operationName());
+    }
+
+    private EvaluationOutcome validateEntity(CriterionSerializer.CriterionEntityEvaluationContext<Job> context) {
+         Job job = context.entity();
+         if (job == null) {
+            return EvaluationOutcome.fail("Job entity is null", StandardEvalReasonCategories.VALIDATION_FAILURE);
+         }
+         // Job is considered successful if status is SUCCEEDED
+         if ("SUCCEEDED".equalsIgnoreCase(job.getStatus())) {
+             return EvaluationOutcome.success();
+         }
+         return EvaluationOutcome.fail("Job status is not SUCCEEDED", StandardEvalReasonCategories.VALIDATION_FAILURE);
+    }
+}
