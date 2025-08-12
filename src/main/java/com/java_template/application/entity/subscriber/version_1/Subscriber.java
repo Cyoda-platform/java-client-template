02d17d0cf695189d12a971b5c541deb8a1bb155d
Ksenniya <lukkonina1@gmail.com>
2025-08-12 10:54:14 -0300

    saved src/main/java/com/java_template/application/entity/subscriber/version_1/Subscriber.java: eab6516a-75e7-11b2-b0f6-36a84c9cbd5c

---
+++ src/main/java/com/java_template/application/entity/subscriber/version_1/Subscriber.java
@@ -0,0 +1,36 @@
+package com.java_template.application.entity.subscriber.version_1;
+
+import com.java_template.common.workflow.CyodaEntity;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.common.ModelSpec;
+import lombok.Data;
+
+@Data
+public class Subscriber implements CyodaEntity {
+    public static final String ENTITY_NAME = "Subscriber";
+    public static final Integer ENTITY_VERSION = 1;
+
+    private String subscriberName;
+    private String contactEmail;
+    private String contactWebhook;
+    private String subscribedCategories;
+
+    public Subscriber() {}
+
+    @Override
+    public OperationSpecification getModelKey() {
+        ModelSpec modelSpec = new ModelSpec();
+        modelSpec.setName(ENTITY_NAME);
+        modelSpec.setVersion(ENTITY_VERSION);
+        return new OperationSpecification.Entity(modelSpec, ENTITY_NAME);
+    }
+
+    @Override
+    public boolean isValid() {
+        boolean hasContact = (contactEmail != null && !contactEmail.isBlank()) ||
+                             (contactWebhook != null && !contactWebhook.isBlank());
+        return subscriberName != null && !subscriberName.isBlank() &&
+               hasContact &&
+               subscribedCategories != null && !subscribedCategories.isBlank();
+    }
+}
