3b4e542499b4bde781ae4a502cb77f2ae78d622e
app-builder <app-builder@example.com>
2025-08-14 08:39:30 +0000

    saved src/main/java/com/java_template/application/entity/user/version_1/User.java: 88d81112-774b-11b2-bb25-0a7418829cad

---
+++ src/main/java/com/java_template/application/entity/user/version_1/User.java
@@ -0,0 +1,37 @@
+package com.java_template.application.entity.user.version_1;
+
+import com.java_template.common.workflow.CyodaEntity;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.common.ModelSpec;
+import lombok.Data;
+
+@Data
+public class User implements CyodaEntity {
+    public static final String ENTITY_NAME = "User";
+    public static final Integer ENTITY_VERSION = 1;
+
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+
+    public User() {}
+
+    @Override
+    public OperationSpecification getModelKey() {
+        ModelSpec modelSpec = new ModelSpec();
+        modelSpec.setName(ENTITY_NAME);
+        modelSpec.setVersion(ENTITY_VERSION);
+        return new OperationSpecification.Entity(modelSpec, ENTITY_NAME);
+    }
+
+    @Override
+    public boolean isValid() {
+        return id != null && !id.isBlank()
+            && name != null && !name.isBlank()
+            && email != null && !email.isBlank()
+            && password != null && !password.isBlank()
+            && role != null && !role.isBlank();
+    }
+}}
