f95806682595f99f60edab7e3a62febe9818c354
Ksenniya <lukkonina1@gmail.com>
2025-08-14 11:55:17 -0300

    saved src/main/java/com/java_template/application/criterion/IngestionFailureCriterion.java: 6291880c-24d6-11b2-a79b-1a87e208ff0a

---
+++ src/main/java/com/java_template/application/criterion/IngestionFailureCriterion.java
@@ -0,0 +1,56 @@
+package com.java_template.application.criterion;
+
+import com.java_template.application.entity.job.version_1.Job;
+import com.java_template.common.serializer.CriterionSerializer;
+import com.java_template.common.serializer.EvaluationOutcome;
+import com.java_template.common.serializer.ReasonAttachmentStrategy;
+import com.java_template.common.serializer.SerializerFactory;
+import com.java_template.common.serializer.StandardEvalReasonCategories;
+import com.java_template.common.workflow.CyodaCriterion;
+import com.java_template.common.workflow.CyodaEventContext;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.processing.EntityCriteriaCalculationRequest;
+import org.cyoda.cloud.api.event.processing.EntityCriteriaCalculationResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IngestionFailureCriterion implements CyodaCriterion {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final CriterionSerializer serializer;
+    private final String className = this.getClass().getSimpleName();
+
+    public IngestionFailureCriterion(SerializerFactory serializerFactory) {
+        this.serializer = serializerFactory.getDefaultCriteriaSerializer();
+    }
+
+    @Override
+    public EntityCriteriaCalculationResponse check(CyodaEventContext<EntityCriteriaCalculationRequest> context) {
+        EntityCriteriaCalculationRequest request = context.getEvent();
+        return serializer.withRequest(request)
+                .evaluateEntity(Job.class, this::validateEntity)
+                .withReasonAttachment(ReasonAttachmentStrategy.toWarnings())
+                .complete();
+    }
+
+    @Override
+    public boolean supports(OperationSpecification modelSpec) {
+        return className.equalsIgnoreCase(modelSpec.operationName());
+    }
+
+    private EvaluationOutcome validateEntity(CriterionSerializer.CriterionEntityEvaluationContext<Job> context) {
+        Job entity = context.entity();
+        if (entity == null) {
+            logger.error("Ingestion failure criterion failed: Job entity is null");
+            return EvaluationOutcome.fail("Job entity is null", StandardEvalReasonCategories.VALIDATION_FAILURE);
+        }
+        if (!"INGESTING".equalsIgnoreCase(entity.getStatus())) {
+            logger.error("Ingestion failure criterion failed: Status is not INGESTING");
+            return EvaluationOutcome.fail("Job status is not INGESTING", StandardEvalReasonCategories.BUSINESS_RULE_FAILURE);
+        }
+        // Further logic can be added to detect failure conditions
+        return EvaluationOutcome.success();
+    }
+}
