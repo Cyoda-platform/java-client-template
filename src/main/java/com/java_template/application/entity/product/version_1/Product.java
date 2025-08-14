f4145044953f3b66bca7cd9f183995abefa5291b
app-builder <app-builder@example.com>
2025-08-14 08:39:32 +0000

    saved src/main/java/com/java_template/application/entity/product/version_1/Product.java: 88d81112-774b-11b2-bb25-0a7418829cad

---
+++ src/main/java/com/java_template/application/entity/product/version_1/Product.java
@@ -0,0 +1,37 @@
+package com.java_template.application.entity.product.version_1;
+
+import com.java_template.common.workflow.CyodaEntity;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.common.ModelSpec;
+import lombok.Data;
+
+@Data
+public class Product implements CyodaEntity {
+    public static final String ENTITY_NAME = "Product";
+    public static final Integer ENTITY_VERSION = 1;
+
+    private String id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer stockQuantity;
+
+    public Product() {}
+
+    @Override
+    public OperationSpecification getModelKey() {
+        ModelSpec modelSpec = new ModelSpec();
+        modelSpec.setName(ENTITY_NAME);
+        modelSpec.setVersion(ENTITY_VERSION);
+        return new OperationSpecification.Entity(modelSpec, ENTITY_NAME);
+    }
+
+    @Override
+    public boolean isValid() {
+        return id != null && !id.isBlank()
+            && name != null && !name.isBlank()
+            && description != null && !description.isBlank()
+            && price != null
+            && stockQuantity != null;
+    }
+}}
