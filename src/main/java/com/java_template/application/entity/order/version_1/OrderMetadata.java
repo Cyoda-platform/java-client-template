19ac37d463a030b0702f471f85c0fa61eec186da
app-builder <app-builder@example.com>
2025-08-15 18:36:13 +0000

    saved src/main/java/com/java_template/application/entity/order/version_1/OrderMetadata.java: b2c41acc-25b9-11b2-a79b-1a87e208ff0a (op-b8f43c44)

---
+++ src/main/java/com/java_template/application/entity/order/version_1/OrderMetadata.java
@@ -0,0 +1,10 @@
+package com.java_template.application.entity.order.version_1;
+
+import lombok.Data;
+
+@Data
+public class OrderMetadata {
+    private String trackingNumber;
+
+    public OrderMetadata() {}
+}
