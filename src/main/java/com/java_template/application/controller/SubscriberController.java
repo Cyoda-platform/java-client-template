126b58e0292a525d604a34cdd728703183c19ced
app-builder <app-builder@example.com>
2025-08-15 20:48:36 +0000

    saved src/main/java/com/java_template/application/controller/SubscriberController.java: 6760f7dc-7878-11b2-bb25-0a7418829cad (op-371750c2)

--- src/main/java/com/java_template/application/controller/SubscriberController.java
+++ src/main/java/com/java_template/application/controller/SubscriberController.java
@@ -23,7 +23,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 import java.net.URI;
 import java.util.NoSuchElementException;
 import java.util.UUID;
