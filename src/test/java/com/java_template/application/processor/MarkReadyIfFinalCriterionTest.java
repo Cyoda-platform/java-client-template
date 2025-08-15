97cf9c6969c38815b4f6b6b22648db5475fb1d7a
Ksenniya <lukkonina1@gmail.com>
2025-08-15 12:03:51 -0300

    saved src/test/java/com/java_template/application/processor/MarkReadyIfFinalCriterionTest.java: 162344ec-259e-11b2-a79b-1a87e208ff0a

---
+++ src/test/java/com/java_template/application/processor/MarkReadyIfFinalCriterionTest.java
@@ -0,0 +1,30 @@
+package com.java_template.application.processor;
+
+import com.java_template.application.entity.game.version_1.Game;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MarkReadyIfFinalCriterionTest {
+
+    @Test
+    public void testFinalStatusUpdatesLastUpdated() throws Exception {
+        SerializerFactory sf = org.mockito.Mockito.mock(SerializerFactory.class);
+        com.java_template.common.serializer.ProcessorSerializer ps = org.mockito.Mockito.mock(com.java_template.common.serializer.ProcessorSerializer.class);
+        org.mockito.Mockito.when(sf.getDefaultProcessorSerializer()).thenReturn(ps);
+
+        MarkReadyIfFinalCriterion processor = new MarkReadyIfFinalCriterion(sf);
+        Game g = new Game();
+        g.setGameId("game-1");
+        g.setDate("2025-03-25");
+        g.setStatus("final");
+
+        Method m = MarkReadyIfFinalCriterion.class.getDeclaredMethod("processEntityLogic", com.java_template.common.serializer.ProcessorSerializer.ProcessorEntityExecutionContext.class);
+        m.setAccessible(true);
+        Game out = (Game) m.invoke(processor, new com.java_template.common.serializer.ProcessorSerializer.ProcessorEntityExecutionContext<>(null, g));
+
+        assertNotNull(out.getLastUpdated());
+    }
+}
