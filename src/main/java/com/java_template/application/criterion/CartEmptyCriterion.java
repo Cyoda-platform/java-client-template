aae33d37a0f1d5aa4801e2ad065f3edac79c87db
Ksenniya <lukkonina1@gmail.com>
2025-08-15 11:45:42 -0300

    saved src/main/java/com/java_template/application/criterion/CartEmptyCriterion.java: 346cec74-259d-11b2-a79b-1a87e208ff0a

---
+++ src/main/java/com/java_template/application/criterion/CartEmptyCriterion.java
@@ -0,0 +1,53 @@
+package com.java_template.application.criterion;
+
+import com.java_template.application.entity.shoppingcart.version_1.ShoppingCart;
+import com.java_template.common.serializer.CriterionSerializer;
+import com.java_template.common.serializer.EvaluationOutcome;
+import com.java_template.common.serializer.ReasonAttachmentStrategy;
+import com.java_template.common.serializer.SerializerFactory;
+import com.java_template.common.serializer.StandardEvalReasonCategories;
+import com.java_template.common.workflow.CyodaCriterion;
+import com.java_template.common.workflow.CyodaEventContext;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.processing.EntityCriteriaCalculationRequest;
+import org.cyoda.cloud.api.event.processing.EntityCriteriaCalculationResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CartEmptyCriterion implements CyodaCriterion {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final CriterionSerializer serializer;
+    private final String className = this.getClass().getSimpleName();
+
+    public CartEmptyCriterion(SerializerFactory serializerFactory) {
+        this.serializer = serializerFactory.getDefaultCriteriaSerializer();
+    }
+
+    @Override
+    public EntityCriteriaCalculationResponse check(CyodaEventContext<EntityCriteriaCalculationRequest> context) {
+        EntityCriteriaCalculationRequest request = context.getEvent();
+        return serializer.withRequest(request)
+            .evaluateEntity(ShoppingCart.class, this::validateEntity)
+            .withReasonAttachment(ReasonAttachmentStrategy.toWarnings())
+            .complete();
+    }
+
+    @Override
+    public boolean supports(OperationSpecification modelSpec) {
+        return className.equalsIgnoreCase(modelSpec.operationName());
+    }
+
+    private EvaluationOutcome validateEntity(CriterionSerializer.CriterionEntityEvaluationContext<ShoppingCart> context) {
+        ShoppingCart cart = context.entity();
+        if (cart == null) {
+            return EvaluationOutcome.fail("Cart is null", StandardEvalReasonCategories.VALIDATION_FAILURE);
+        }
+        if (cart.getItems() != null && !cart.getItems().isEmpty()) {
+            return EvaluationOutcome.fail("Cart is not empty", StandardEvalReasonCategories.VALIDATION_FAILURE);
+        }
+        return EvaluationOutcome.success();
+    }
+}
