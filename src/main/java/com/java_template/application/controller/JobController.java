7b88db713b23e2379ee41611c6aebe11c01e85aa
app-builder <app-builder@example.com>
2025-08-18 14:27:25 +0000

    saved src/main/java/com/java_template/application/controller/JobController.java: 4d6635aa-7aa2-11b2-bb25-0a7418829cad (op-76458cbe)

--- src/main/java/com/java_template/application/controller/JobController.java
+++ src/main/java/com/java_template/application/controller/JobController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestBody as SpringRequestBody; // aliasing for clarity
 
 import java.util.NoSuchElementException;
 import java.util.UUID;
