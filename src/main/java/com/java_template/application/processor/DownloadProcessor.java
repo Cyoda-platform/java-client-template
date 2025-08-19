e404c61a013a61404f7f7281967269c82b00c971
app-builder <app-builder@example.com>
2025-08-19 21:16:15 +0000

    saved src/main/java/com/java_template/application/processor/DownloadProcessor.java: 38c338d0-7ba3-11b2-bb25-0a7418829cad (op-bd44055c)

--- src/main/java/com/java_template/application/processor/DownloadProcessor.java
+++ src/main/java/com/java_template/application/processor/DownloadProcessor.java
@@ -18,7 +18,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.util.concurrent.CompletableFuture;
 
