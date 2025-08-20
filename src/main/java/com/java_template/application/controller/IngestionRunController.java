4b1a85bf5cf051b42ca68ae5083b4a56ab3cf683
app-builder <app-builder@example.com>
2025-08-20 14:32:28 +0000

    saved src/main/java/com/java_template/application/controller/IngestionRunController.java: 1203e4d4-7c33-11b2-bb25-0a7418829cad (op-7f567900)

--- src/main/java/com/java_template/application/controller/IngestionRunController.java
+++ src/main/java/com/java_template/application/controller/IngestionRunController.java
@@ -17,7 +17,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.annotations.parameters.RequestBody as SwaggerRequestBody;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.util.UUID;
