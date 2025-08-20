bd2da17db8df632100c54670ced277c7fa7c029c
app-builder <app-builder@example.com>
2025-08-20 08:30:02 +0000

    saved src/main/java/com/java_template/application/entity/product/version_1/Product.java: b6bf6bea-7bfe-11b2-bb25-0a7418829cad (op-de04cb1a)

--- src/main/java/com/java_template/application/entity/product/version_1/Product.java
+++ src/main/java/com/java_template/application/entity/product/version_1/Product.java
@@ -1,23 +1,25 @@
-package com.java_template.application.entity.product.version_1; // replace {entityName} with actual entity name in lowercase
+package com.java_template.application.entity.product.version_1;
 
 import com.java_template.common.workflow.CyodaEntity;
 import com.java_template.common.workflow.OperationSpecification;
 import org.cyoda.cloud.api.event.common.ModelSpec;
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 public class Product implements CyodaEntity {
     public static final String ENTITY_NAME = "Product";
     public static final Integer ENTITY_VERSION = 1;
-    // Add your entity fields here
 
+    // Add your entity fields here
     private String sku;
     private String name;
     private String description;
     private Double price;
     private Integer quantityAvailable;
-    private String category; // optional
-    private String imageUrl; // optional
+    private String category;
+    private String imageUrl;
     private Boolean active;
     private String created_at;
     private String updated_at;
