0d99d6bd74556e06246231c74fd8769369e87266
app-builder <app-builder@example.com>
2025-08-20 13:35:26 +0000

    saved src/main/java/com/java_template/application/processor/ValidateJobProcessor.java: a08b9074-297e-11b2-a79b-1a87e208ff0a (op-87e1b470)

--- src/main/java/com/java_template/application/processor/ValidateJobProcessor.java
+++ src/main/java/com/java_template/application/processor/ValidateJobProcessor.java
@@ -12,6 +12,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 public class ValidateJobProcessor implements CyodaProcessor {
 
