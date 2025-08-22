4132717e5a5f4abc564feaa3180a28d2290ee104
app-builder <app-builder@example.com>
2025-08-22 15:39:47 +0000

    saved src/main/java/com/java_template/application/entity/Subscriber.java: dc54025e-5fd8-11b2-af30-2a39b88383d3

---
+++ src/main/java/com/java_template/application/entity/Subscriber.java
@@ -0,0 +1,33 @@
+package com.java_template.application.entity;
+
+import com.java_template.common.workflow.CyodaEntity;
+import com.java_template.common.workflow.OperationSpecification;
+import org.cyoda.cloud.api.event.common.ModelSpec;
+import lombok.Data;
+import static com.java_template.common.config.Config.ENTITY_VERSION;
+
+@Data
+public class Subscriber implements CyodaEntity {
+    public static final String ENTITY_NAME = "Subscriber";
+
+    private String contactType;
+    private String contactValue;
+    private Boolean active;
+
+    public Subscriber() {}
+
+    @Override
+    public OperationSpecification getModelKey() {
+        ModelSpec modelSpec = new ModelSpec();
+        modelSpec.setName(ENTITY_NAME);
+        modelSpec.setVersion(Integer.parseInt(ENTITY_VERSION));
+        return new OperationSpecification.Entity(modelSpec, ENTITY_NAME);
+    }
+
+    @Override
+    public boolean isValid() {
+        return contactType != null && !contactType.isBlank()
+                && contactValue != null && !contactValue.isBlank()
+                && active != null;
+    }
+}
