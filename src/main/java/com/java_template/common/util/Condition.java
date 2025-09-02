1d87f4cb584d26cf4d6ce94b790808bf52f6535d
Ksenniya <lukkonina1@gmail.com>
2025-09-01 23:34:39 -0300

    updated requirement

--- src/main/java/com/java_template/common/util/Condition.java
+++ src/main/java/com/java_template/common/util/Condition.java
@@ -1,24 +1,43 @@
 package com.java_template.common.util;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Condition {
     private String type;
+
+    // For simple conditions
     private String jsonPath;
-    private String operator;
+
+    // For lifecycle conditions
+    private String field;
+
+    // Map the expected 'operatorType' from the request to 'operation' for Cyoda API
+    @JsonProperty("operation")
+    private String operatorType;
+
     private Object value;
 
-    public Condition(String type, String jsonPath, String operatorType, Object value) {
+    public Condition(String type, String jsonPath, String field, String operatorType, Object value) {
         this.type = type;
         this.jsonPath = jsonPath;
-        this.operator = operatorType;
+        this.field = field;
+        this.operatorType = operatorType;
         this.value = value;
     }
 
+    // For simple conditions
     public static Condition of(String jsonPath, String operatorType, Object value) {
-        return new Condition("simple", jsonPath, operatorType, value);
+        return new Condition("simple", jsonPath, null, operatorType, value);
+    }
+
+    // For lifecycle conditions
+    public static Condition lifecycle(String field, String operatorType, Object value) {
+        return new Condition("lifecycle", null, field, operatorType, value);
     }
 }
